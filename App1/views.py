ef379160aae8b33915125a7edb08a1f5ecf82584
Your Name <you@example.com>
2022-01-18 17:43:54 -0500

    Home page update for final project

--- App1/views.py
+++ App1/views.py
@@ -1,10 +1,11 @@
 import datetime
+import pytz #library to offset date time
 
 from django.shortcuts import render
 
 
 def home(request):
     data = dict()
-    now = datetime.datetime.now()
+    now = datetime.utcnow().replace(tzinfo=pytz.utc)
     data["time_now"] = now
     return render(request, "Home.html", context = data)
