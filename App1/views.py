5cb317328d70897793e0f4f01943a78511e85f97
Your Name <you@example.com>
2022-01-18 15:26:47 -0500

    test2

--- App1/views.py
+++ App1/views.py
@@ -7,4 +7,4 @@ def home(request):
     data = dict()
     now = datetime.datetime.now()
     data["time_now"] = now
-    return render(request, "home.html", context = data)
+    return render(request, "Home.html", context = data)
